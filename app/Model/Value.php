81d2179db7a0d2d0a5acf089545ab421209cdb3c
Sami Balouki <sbalouki33@gmail.com>
2019-02-15 13:26:39 +0100

    Creation of Value model and resource controller. Creation of the app main route.

---
+++ app/Model/Value.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Value extends Model
+{
+    /**
+     * The attributes that should be visible in arrays.
+     *
+     * @var array
+     */
+    protected $fillable = ['value'];
+}
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,10 +10,11 @@
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
+
     </head>
     <body>
         <div class="container">
-            @yield('content')
+            {{$value}}
         </div>
     </body>
 </html>
